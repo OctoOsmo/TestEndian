06eedfa31c3cc4ba282dc11ffa91ed73332d3593
OctoOsmo <tatarincev_al@bk.ru>
2013-05-20 03:52:21 +0400

    output to screen first symbols of codepage

--- Test endian/Test endian.cpp
+++ Test endian/Test endian.cpp
@@ -2,9 +2,13 @@
 
 int main(void)
 {
-	short x = 1;
+	short x = 0x01;
 	printf("your endian is: %s\n", ((char) x == 0) ? "big endian" : "little endian");
 	printf("x = %c\n", (char) x);
+	for(int i = 1; i < 50; ++i)
+	{
+		printf("symbol ¹%d = %c\n", i, (char) i);
+	}
 	getchar();
 	return 0;
 }
