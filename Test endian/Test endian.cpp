f2308e4d258bf038d948b3a19553096819d0df8c
OctoOsmo <tatarincev_al@bk.ru>
2013-05-19 23:02:58 +0400

    remove more useless code

--- Test endian/Test endian.cpp
+++ Test endian/Test endian.cpp
@@ -1,16 +1,8 @@
-// Test endian.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
 #include "stdio.h"
 
-
-//int _tmain(int argc, _TCHAR* argv[])
 int main(void)
 {
 	short x = 1;
-	//char str;
-	//str = ((char) x = 0
 	printf("your endian is: %s\n", ((char) x == 0) ? "big endian" : "little endian");
 	getchar();
 	return 0;
